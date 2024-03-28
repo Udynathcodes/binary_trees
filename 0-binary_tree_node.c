2e62ab483b32864780eabe1a8b6a388e3b0c98f3
Uduak-obong Nathan Uwah <136357015+Udynathcodes@users.noreply.github.com>
2024-03-27 18:40:52 -0700

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - creates a binary tree node
-* @parent: pointer to the parent node of the node to create
+* binary_tree_node - creates a binary tree
+* @parent: pointer to the node of a binary tree
 * @value: the value to put in the new node
 *
-* Return: a pointer to the new node, or NULL on failure
+* Return: pointer to the new node, or NULL if fail
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
